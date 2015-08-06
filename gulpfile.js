b50dc250a172ba55c2c202dc7f50580b06405f82
sam <samlombardi86@gmail.com>
2015-08-06 10:01:52 +0200

    Clean task + single files pick

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
+'use strict';
 
-gulp.task('default', function () {
-  return gulp.src('./js/*.js')
+let gulp = require('gulp');
+let concat = require('gulp-concat');
+let del = require('del');
+
+gulp.task('clean', function (done) {
+  del(['dist'], done);
+});
+
+gulp.task('default', ['clean'], function () {
+  return gulp.src(['./js/jquery.ui.widget.js', './js/jquery.iframe-transport.js', './js/jquery.fileupload.js', './js/jquery.cloudinary.js'])
     .pipe(concat('main.js'))
     .pipe(gulp.dest('./dist'));
 });
